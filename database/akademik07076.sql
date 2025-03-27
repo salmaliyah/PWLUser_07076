2cc70d2282cb7b35f1e5d2a633a22891b6466a87
Salma Fatin <salmafatin151005@gmail.com>
2025-03-27 08:42:42 +0700

    first commit

---
+++ database/akademik07076.sql
@@ -0,0 +1,75 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 24 Mar 2025 pada 07.50
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.1.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `akademik07076`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user`
+--
+
+CREATE TABLE `user` (
+  `iduser` int(11) NOT NULL,
+  `username` varchar(50) DEFAULT NULL,
+  `password` varchar(32) DEFAULT NULL,
+  `status` varchar(5) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data untuk tabel `user`
+--
+
+INSERT INTO `user` (`iduser`, `username`, `password`, `status`) VALUES
+(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'admin'),
+(2, 'mhs', '0357a7592c4734a8b1e12bc48e29e9e9', 'mhs'),
+(3, 'dsn', 'da532bf806defa26fdbeee5dd2e0d68f', 'dsn'),
+(4, 'salma', 'salma0909', 'mhs'),
+(9, 'salma fatin', '56789', 'mhs'),
+(10, 'a122307076', '090877', 'mhs'),
+(13, 'a122307076', '1234', 'mhs'),
+(14, 'a122307076', '23456', 'mhs'),
+(15, 'salmafatin', '1516278', 'TU');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`iduser`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `user`
+--
+ALTER TABLE `user`
+  MODIFY `iduser` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ fungsi.php
@@ -0,0 +1,21 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "akademik07076";
+
+// Create connection
+ 
+$koneksi = mysqli_connect($servername, $username, $password, $database);
+ 
+// Check connection
+ 
+if (!$koneksi) {
+ 
+    die("Connection failed: " . mysqli_connect_error());
+ 
+}
+//echo "Connected successfully";
+//mysqli_close($conn);
+?>
